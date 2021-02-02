2e86cad20155566c22975e81f86614c66c990ab6
loloffs <lukeoloffs@gmail.com>
2021-02-02 21:00:59 +0000

    Added assistance request queries

---
+++ 4_queries/total_cohort_assistance_duration.sql
@@ -0,0 +1,23 @@
+SELECT cohorts.name as cohort, sum(completed_at-started_at) as total_duration
+FROM assistance_requests
+JOIN students ON students.id = student_id
+JOIN cohorts on cohorts.id = cohort_id
+GROUP BY cohorts.name
+ORDER BY total_duration;
+
+-- Get the total duration of all assistance requests for each cohort.
+
+-- Select the cohort's name and the total duration the assistance requests.
+-- Order by total_duration.
+-- Look at the ERD to see how to join the tables.
+-- Expected Result:
+
+--  cohort | total_duration 
+-- --------+----------------
+--  JUL30  | 390:35:20
+--  AUG27  | 398:19:00
+--  JUL02  | 453:50:30
+--  NOV19  | 462:34:40
+--  MAY07  | 480:10:55
+--  ...
+--  (11 rows)
