ffdcc34fd4633d1b644bdbf6922b4776b68600d8
loloffs <lukeoloffs@gmail.com>
2021-02-04 02:32:54 +0000

    Parameterized my queries to avoid SQL injection attack

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,17 +8,25 @@ const pool = new Pool({
 });
 
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`;
+
+
+
+const cohortName = process.argv[2];
+const value = [`%${cohortName}%`];
+
+
+pool.query(queryString, value)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
-});
+}).catch(err => console.error('query error', err.stack));
